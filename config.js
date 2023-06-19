e408cfc50255c098a745d77b076212e7c1ff1807
ouss227T7EE <115144112+ouss227T7EE@users.noreply.github.com>
2023-06-19 18:49:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'og', true], 
+  ['212777676587', 'sandaru', true],
+  ['6281255369012', 'ash', true], 
   [''] 
 ] //Number of owners
 
